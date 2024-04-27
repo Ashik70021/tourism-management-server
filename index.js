2e9118731d210fd3dfdbf3def506c6cbb46755d8
Ashik70021 <ashik70021@gmail.com>
2024-04-27 14:01:13 +0600

    implement update and delete section

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpk7gxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -48,6 +48,41 @@ async function run() {
             console.log(req.params.email);
             const result = await spotCollection.find({user_email: req.params.email}).toArray();
             res.send(result)
+        })
+
+        app.get("/singleSpot/:id", async (req, res) =>{
+            console.log(req.params.id);
+            const result = await spotCollection.findOne({
+                _id: new ObjectId(req.params.id),
+            });
+            console.log(result);
+            res.send(result);
+        })
+
+        app.put("/updateProduct/:id", async(req,res)=>{
+            console.log(req.params.id)
+            const query = {_id: new ObjectId(req.params.id)};
+            const data = {
+                $set: {
+                    country_Name: req.body.country_Name,
+                    tourists_spot_name: req.body.tourists_spot_name,
+                    image: req.body.image,
+                    location: req.body.location,
+                    short_description: req.body.short_description,
+                    seasonality: req.body.seasonality,
+                    totalVisitorsPerYear: req.body.totalVisitorsPerYear,
+                }
+            }
+            const result = await spotCollection.updateOne(query, data);
+            console.log(result)
+            res.send(result)
+        })
+
+        app.delete("/delete/:id", async (req,res) =>{
+            const result = await  spotCollection.deleteOne({
+                _id: new ObjectId(req.params.id) })
+                console.log(result);
+                res.send(result)
         })
     } finally {
         // Ensures that the client will close when you finish/error
